845b30b2f2a62b8bafed53e01d87cfb496f79677
JhoanSGomez <jsgomezt_1@uqvirtual.edu.co>
2024-04-11 22:26:54 -0500

    Create scene questions level 4 and changed in storeLevel4

---
+++ Assets/Scripts/level4/DialogLevel4.cs
@@ -0,0 +1,158 @@
+using System.Collections;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class DialogLevel4 : MonoBehaviour
+{
+
+    //[SerializeField] private AudioClip npcVoice;
+    public AudioClip audioClip1;
+    public AudioClip audioClip2;
+    public AudioClip audioClip3;
+    [SerializeField] private float typingTime;
+    [SerializeField] private GameObject dialogMark;
+    [SerializeField] private GameObject dialogPanel;
+    [SerializeField] private TMP_Text dialogText;
+    [SerializeField, TextArea(4, 6)] private string[] dialogLines;
+    private bool isPlayerInRange;
+    private bool didDialogStart;
+    private int lineIndex;
+    private int flag = 0;
+    public AudioSource audioSource;
+
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        //audioSource.clip = npcVoice;
+
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPlayerInRange && Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!didDialogStart)
+            {
+                startDialog();
+            }
+            else if (dialogText.text == dialogLines[lineIndex])
+            {
+                NextDialogLine();
+
+                if (lineIndex == 2)
+                {
+                    StartCoroutine(waitAndLoad(0.5F));
+                }
+            }
+                else if(PlayerPrefs.GetInt("flagDialogLevel4")==1)
+               {
+                   StopAllCoroutines();
+                   dialogText.text = dialogLines[lineIndex];
+                   StartCoroutine(waitAndLoad(0.5F));
+                   }
+                
+
+        }
+    }
+    IEnumerator waitAndLoad(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        SceneManager.LoadScene("DialogueMentorLevel4");
+        PlayerPrefs.SetInt("flagDialogLevel4", 1);
+    }
+
+    private void startDialog()
+    {
+        didDialogStart = true;
+        dialogPanel.SetActive(true);
+        dialogMark.SetActive(false);
+        lineIndex = 0;
+        Time.timeScale = 0f;
+        StartCoroutine(ShowLine());
+    }
+
+    private void NextDialogLine()
+    {
+        lineIndex++;
+        if (lineIndex < dialogLines.Length)
+        {
+            StartCoroutine(ShowLine());
+        }
+        else
+        {
+            didDialogStart = false;
+            dialogPanel.SetActive(false);
+            dialogMark.SetActive(true);
+            Time.timeScale = 1f;
+        }
+    }
+
+    private IEnumerator ShowLine()
+    {
+        dialogText.text = string.Empty;
+        //audioSource.Play();
+
+        switch (flag)
+        {
+            case 0:
+                PlayAudioClip(audioClip1);
+                flag++;
+                break;
+            case 1:
+                PlayAudioClip(audioClip2);
+                flag++;
+                break;
+            case 2:
+                PlayAudioClip(audioClip3);
+                flag++;
+                break;
+            default:
+                Debug.Log("Opción no válida");
+                break;
+        }
+
+        foreach (char ch in dialogLines[lineIndex])
+        {
+            dialogText.text += ch;
+            yield return new WaitForSecondsRealtime(typingTime);
+        }
+
+        if (flag >= dialogLines.Length)
+        {
+            flag = 0;
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isPlayerInRange = true;
+            dialogMark.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isPlayerInRange = false;
+            dialogMark.SetActive(false);
+        }
+    }
+
+    void PlayAudioClip(AudioClip clip)
+    {
+        // Asignar el nuevo audioClip al AudioSource y reproducir
+        audioSource.clip = clip;
+        audioSource.Play();
+    }
+}
